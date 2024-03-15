e0e695162fd05e5424a83e8f455ba8d351e8046d
Rui <ruicarlos3g.rc@gmail.com>
2024-03-14 21:15:57 -0300

    feature: adicionar entidade livro

--- backend/src/main/java/br/com/agora/entity/Livro.java
+++ backend/src/main/java/br/com/agora/entity/Livro.java
@@ -1,5 +1,62 @@
 package br.com.agora.entity;
 
-public class Livro {
+import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter @Setter
+@Entity
+@Table(name="tb_livro")
+public class Livro {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String isbn;
+	
+	private String titulo;
+	
+	private String autor;
+	
+	private String sinope;
+	
+	private String editora;
+	
+	private String idioma;
+	
+	private String categoria;
+	
+	private int numeroPagina;
+	
+	private Date dataPublicacao;
+	
+	private String dimensao;
+	
+	private double peso;
+	
+	private double precoFisico;
+	
+	private int qtdEstoque;
+	
+	private String arquivoDigital;
+	
+	private double precoDigital;
+	
+	private String capaLivro;
+	
+	private Boolean livroDigital;
+	
+	private Boolean livroFisico;
+	
+	private Livro() {};
+
+	
 }
