b6b374177687b6307fad02c4cf6d76dab653dc43
Emannuel Morais <emanuelmorais2015@gmail.com>
2024-03-14 21:22:25 -0300

    criacao da classe Usuario.java UsuarioRepository.java

--- backend/src/main/java/br/com/agora/entity/Usuario.java
+++ backend/src/main/java/br/com/agora/entity/Usuario.java
@@ -1,9 +1,37 @@
 package br.com.agora.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter @Setter
-public class Usuario {
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "cpf_cnpj", nullable = false, unique = true)
+    private String cpfCpnj;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
 
 }
---
+++ backend/src/main/java/br/com/agora/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.com.agora.repository;
+
+import br.com.agora.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
