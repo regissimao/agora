6eabe8f4974fe4c20dbfdc49c23a43dafa276c4d
Emannuel Morais <emanuelmorais2015@gmail.com>
2024-03-14 22:59:16 -0300

    criando a entity Usuario.java

--- backend/src/main/java/br/com/agora/entity/Usuario.java
+++ backend/src/main/java/br/com/agora/entity/Usuario.java
@@ -1,9 +1,33 @@
 package br.com.agora.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Getter @Setter
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_usuarios")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "cpf_cnpj", nullable = false, unique = true)
+    private String cpfCpnj;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
 
 }
