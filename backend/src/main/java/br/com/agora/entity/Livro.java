82872723991c38e8d1cc0ccae8dbfc5a1514abd4
Alano Morais <alanomorais@gmail.com>
2024-03-21 19:56:38 -0300

    definir colunas entity Livros

--- backend/src/main/java/br/com/agora/entity/Livro.java
+++ backend/src/main/java/br/com/agora/entity/Livro.java
@@ -2,6 +2,7 @@ package br.com.agora.entity;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,40 +21,55 @@ public class Livro {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
+	@Column(name = "isbn", nullable = false)
 	private String isbn;
 	
+	@Column(name = "titulo", nullable = false)
 	private String titulo;
 	
+	@Column(name = "autor", nullable = false)
 	private String autor;
 	
 	private String sinope;
 	
+	@Column(name = "editora", nullable = false)
 	private String editora;
 	
+	@Column(name = "idioma", nullable = false)
 	private String idioma;
 	
+	@Column(name = "categoria", nullable = false)
 	private String categoria;
 	
+	@Column(name = "numero_pagina", nullable = false)
 	private int numeroPagina;
 	
+	@Column(name = "data_publicacao")
 	private Date dataPublicacao;
 	
 	private String dimensao;
 	
 	private double peso;
 	
+	@Column(name = "peso_fisico")
 	private double precoFisico;
 	
-	private int qtdEstoque;
+	@Column(name = "quantidade_estoque")
+	private int quantidadeEstoque;
 	
+	@Column(name = "arquivo_digital")
 	private String arquivoDigital;
 	
+	@Column(name = "preco_digital")
 	private double precoDigital;
 	
+	@Column(name = "capa_livro")
 	private String capaLivro;
 	
+	@Column(name = "livro_digital")
 	private Boolean livroDigital;
 	
+	@Column(name = "livro_fisico")
 	private Boolean livroFisico;
 	
 	private Livro() {};
