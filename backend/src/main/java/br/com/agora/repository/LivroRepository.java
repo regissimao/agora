be03f13645117b57a9fc60bbd29ffbaf966cede3
Emannuel Morais <emanuelmorais2015@gmail.com>
2024-03-21 21:23:25 -0300

    criando livro repository

---
+++ backend/src/main/java/br/com/agora/repository/LivroRepository.java
@@ -0,0 +1,7 @@
+package br.com.agora.repository;
+
+import br.com.agora.entity.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
