6a6003b0863e5b61551307eb18080965f4c1edbc
Régis Simão <regissimao@yahoo.com.br>
2024-03-21 21:15:42 -0300

    Correção de uns imports

--- backend/src/main/java/br/com/agora/entity/Pagamento.java
+++ backend/src/main/java/br/com/agora/entity/Pagamento.java
@@ -10,11 +10,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name="tb_pagamento")
