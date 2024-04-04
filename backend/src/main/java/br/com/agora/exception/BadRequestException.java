76d96c849d7426d47b3a3b048227bdf4d9b14ac6
evertonfilhoo23 <evertonaraujo0513@gmail.com>
2024-04-03 22:53:58 -0300

    FLuxo completo de resumo de pedido

--- backend/src/main/java/br/com/agora/exception/BadRequestException.java
+++ backend/src/main/java/br/com/agora/exception/BadRequestException.java
@@ -1,7 +1,6 @@
 package br.com.agora.exception;
 
 public class BadRequestException extends RuntimeException{
-    
     private static final long serialVersionUID = -334234234234234L;
 
     public BadRequestException(String message) {
