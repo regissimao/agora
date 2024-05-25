33565cfd2c0ea072b724f430631bee87a9f02062
Matheus9819 <matheus.100.silva@gmail.com>
2024-05-24 22:28:02 -0300

    Criação do front da tela cadastro de clientes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
+import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'pagina-inicial', component: PaginaInicialComponent},
+  {path: 'logar', component: LoginComponent},
+  {path: '', redirectTo: 'logar', pathMatch: 'full'},
+  {path: 'cadastro-usuario', component: CadastroUsuarioComponent},
+];
---
+++ frontend/src/app/cadastro-usuario/cadastro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.css'
+})
+export class CadastroUsuarioComponent {
+
+}
